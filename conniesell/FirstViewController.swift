9a9dd999d15ecd9b8ff86d587f84f9045dcfda43
Yolanda H <huangyalan@yolanda-h-macbook-air.local>
2018-11-12 05:08:48 +0800

    Initial Commit

---
+++ conniesell/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  conniesell
+//
+//  Created by Yolanda H. on 2018/11/12.
+//  Copyright © 2018 Yolanda H. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ conniesell/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  conniesell
+//
+//  Created by Yolanda H. on 2018/11/12.
+//  Copyright © 2018 Yolanda H. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
